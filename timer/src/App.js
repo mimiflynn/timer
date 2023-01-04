7d54b9956d9fbd85e1ef8026ecc5c0a519e01aab
Mimi <mimiflynn@gmail.com>
2023-01-04 12:42:17 -0500

    lint fix and audit fix

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import classNames from 'classnames';
-import './styles/App.css';
-import { DEFAULT_LIMIT, DEFAULT_WARNING } from './constants';
-
+import React, { useEffect, useState } from "react";
+import classNames from "classnames";
+import "./styles/App.css";
+import { DEFAULT_LIMIT, DEFAULT_WARNING } from "./constants";
 
 function timer(timeElapsed) {
   if (timeElapsed) {
@@ -15,12 +14,16 @@ function timer(timeElapsed) {
 function formatTime(elapsedSeconds) {
   const minutes = Math.floor(elapsedSeconds / 60);
   const seconds = elapsedSeconds % 60;
-  return minutes + ':' + ((seconds < 10) ? '0' + seconds.toString() : seconds.toString());
+  return (
+    minutes +
+    ":" +
+    (seconds < 10 ? "0" + seconds.toString() : seconds.toString())
+  );
 }
 
 function convertToSeconds(time) {
-  const value = time.split(':');
-  return (Number(value[0]) * 60) + Number(value[1]);
+  const value = time.split(":");
+  return Number(value[0]) * 60 + Number(value[1]);
 }
 
 function App() {
@@ -36,9 +39,9 @@ function App() {
       }, 1000);
       return () => clearTimeout(timeout);
     } else {
-      setTime(timeElapsed)
+      setTime(timeElapsed);
     }
-  });
+  }, [setTime, timeElapsed, timerStarted]);
 
   function handleLimitUpdate(event) {
     setTimeLimit(convertToSeconds(event.target.value));
@@ -53,17 +56,21 @@ function App() {
       <div className="">
         <button
           type="button"
-          className={classNames('btn', {
-            'btn-primary': !timerStarted,
-            'btn-danger': timerStarted
+          className={classNames("btn", {
+            "btn-primary": !timerStarted,
+            "btn-danger": timerStarted,
           })}
-          onClick={() => setTimerStatus(!timerStarted)}>
-          {timerStarted ? 'Stop' : 'Start'}
+          onClick={() => setTimerStatus(!timerStarted)}
+        >
+          {timerStarted ? "Stop" : "Start"}
         </button>
         <button
           type="button"
           className="btn btn-dark"
-          onClick={() => setTime(0)}>Reset</button>
+          onClick={() => setTime(0)}
+        >
+          Reset
+        </button>
       </div>
     );
   }
@@ -74,15 +81,17 @@ function App() {
         <div className="row">
           <div className="col">
             <div className="input-group">
-              <span className="input-group-text"
-                id="time-limit">Limit</span>
+              <span className="input-group-text" id="time-limit">
+                Limit
+              </span>
               <input
                 type="text"
                 className="form-control"
                 aria-label="Set Time Limit"
                 aria-describedby="time-limit"
                 placeholder={formatTime(timeLimit)}
-                onChange={handleLimitUpdate}></input>
+                onChange={handleLimitUpdate}
+              ></input>
             </div>
           </div>
           <div className="col">
@@ -93,9 +102,11 @@ function App() {
                 aria-label="Set Time Warning"
                 aria-describedby="time-warning"
                 placeholder={formatTime(timeWarning)}
-                onChange={handleWarningUpdate}></input>
-              <span className="input-group-text"
-                id="time-limit">Warning</span>
+                onChange={handleWarningUpdate}
+              ></input>
+              <span className="input-group-text" id="time-limit">
+                Warning
+              </span>
             </div>
           </div>
         </div>
@@ -105,17 +116,17 @@ function App() {
 
   return (
     <div className="position-relative">
-      <div className={classNames('timer', {
-        'warning': timeElapsed > (timeLimit - timeWarning),
-        'stop': timeElapsed > timeLimit
-      })}>
-        <div className="time">
-          {formatTime(timeElapsed)}
-        </div>
+      <div
+        className={classNames("timer", {
+          warning: timeElapsed > timeLimit - timeWarning,
+          stop: timeElapsed > timeLimit,
+        })}
+      >
+        <div className="time">{formatTime(timeElapsed)}</div>
         {renderControls()}
       </div>
       {renderSettings()}
-    </div >
+    </div>
   );
 }
 

f604770def25055f6a7abff67a014b5b80eabe66
Mimi Flynn <414934+mimiflynn@users.noreply.github.com>
2023-01-31 10:24:30 -0500

    GH Actions (#2)
    * flatten project
    
    * gh-actions test
    
    * actions test
    
    * more test
    
    * branches
    
    * passing test
    
    * remove concurrency
    
    * basic
    
    * more basic
    
    * only modern node
    
    * path
    
    * v3
    
    * scripts
    
    * more
    
    * cleanup

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders start time on init', async () => {
+  render(<App />);
+  const timeElement = await screen.findAllByText(/0:00/i);
+  expect(timeElement[0]).toBeTruthy();
+});

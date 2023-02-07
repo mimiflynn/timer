3d180127efd3f97f6190eda86835c51977cfabdf
Mimi Flynn <414934+mimiflynn@users.noreply.github.com>
2023-02-07 10:51:11 -0500

    use font package (#6)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
-import './styles/dseg.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
